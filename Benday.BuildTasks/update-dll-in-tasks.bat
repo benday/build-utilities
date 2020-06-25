fa4c492343f6bd11c759c2a38e4e92cbdda88cfc
Benjamin Day <benday@benday.com>
2020-06-25 14:34:23 -0400

    updated dll versions and cleaned up extra included binary files.

--- Benday.BuildTasks/update-dll-in-tasks.bat
+++ Benday.BuildTasks/update-dll-in-tasks.bat
@@ -2,7 +2,5 @@ set VersionToInclude=netcoreapp3.1
 set FromPath=..\Benday.BuildUtilities.Core\published
 set FileOrFilesToCopy=*.*
 
-copy %FromPath%\%FileOrFilesToCopy% .\setappconfigappsetting
-copy %FromPath%\%FileOrFilesToCopy% .\setappconfigconnectionstring
-rem copy %FromPath%\%FileOrFilesToCopy% .\setjsonconfigconnectionstring
-rem copy %FromPath%\%FileOrFilesToCopy% .\setjsonvalue
+xcopy /e /y %FromPath%\ .\setappconfigappsetting
+xcopy /e /y %FromPath%\ .\setappconfigconnectionstring
