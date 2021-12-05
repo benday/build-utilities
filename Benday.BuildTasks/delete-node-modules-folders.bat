0f4e4cf83e724067127ec2f807baca2bd7ffca6c
Benjamin Day <benday@benday.com>
2021-12-05 09:19:35 -0500

    added delete node folders batch file

---
+++ Benday.BuildTasks/delete-node-modules-folders.bat
@@ -0,0 +1,5 @@
+rd /s /q .\deployefcoremigrations\node_modules
+rd /s /q .\runsqlcmd\node_modules
+rd /s /q .\setappconfigappsetting\node_modules
+rd /s /q .\setappconfigconnectionstring\node_modules
+rd /s /q .\setjsonconfigconnectionstring\node_modules
