384f8c4e509f61f399d651b927b141235d86a86a
benday <benday@benday.com>
2024-05-22 14:27:39 -0400

    starting impl of set arrays from build

--- Benday.BuildTasks/setjsonvalue/index.ts
+++ Benday.BuildTasks/setjsonvalue/index.ts
@@ -10,8 +10,19 @@ async function run(): Promise<void> {
     try {
         const numberOfLevels: string = tl.getInput("numberOfLevels", true);
         const filename = tl.getInput("filename", true);
+        let setValueAsArray = tl.getInput("setValueAsArray", false);
         let valueToSet = tl.getInput("valueToSet", false);
+        let valueToSet2 = tl.getInput("valueToSet2", false);
+        let valueToSet3 = tl.getInput("valueToSet3", false);
+        let valueToSet4 = tl.getInput("valueToSet4", false);
+        let valueToSet5 = tl.getInput("valueToSet5", false);
+        let valueToSet6 = tl.getInput("valueToSet6", false);
+        let valueToSet7 = tl.getInput("valueToSet7", false);
+        let valueToSet8 = tl.getInput("valueToSet8", false);
+        let valueToSet9 = tl.getInput("valueToSet9", false);
+
         const key1 = tl.getInput("keyname1", true);
+
 
         if (valueToSet === undefined || valueToSet === null) {
             valueToSet = '';
@@ -40,8 +51,51 @@ async function run(): Promise<void> {
             const key3 = tl.getInput("keyname3", true);
             const key4 = tl.getInput("keyname4", true);
 
-            editor.setValue(valueToSet, key1, key2, key3, key4);
-            editor.save(filename);
+            if (setValueAsArray === "true") {
+                var values: string[] = [];
+
+                if (!isEmpty(valueToSet)) {
+                    values.push(valueToSet);
+                }
+
+                if (!isEmpty(valueToSet2)) {
+                    values.push(valueToSet2);
+                }
+
+                if (!isEmpty(valueToSet3)) {
+                    values.push(valueToSet3);
+                }
+
+                if (!isEmpty(valueToSet4)) {
+                    values.push(valueToSet4);
+                }
+
+                if (!isEmpty(valueToSet5)) {
+                    values.push(valueToSet5);
+                }
+
+                if (!isEmpty(valueToSet6)) {
+                    values.push(valueToSet6);
+                }
+
+                if (!isEmpty(valueToSet7)) {
+                    values.push(valueToSet7);
+                }
+
+                if (!isEmpty(valueToSet8)) {
+                    values.push(valueToSet8);
+                }
+
+                if (!isEmpty(valueToSet9)) {
+                    values.push(valueToSet9);
+                }
+
+                editor.setValueAsArray(values, key1, key2, key3, key4);
+                editor.save(filename);
+            } else {
+                editor.setValue(valueToSet, key1, key2, key3, key4);
+                editor.save(filename);
+            }
         }
     } catch (err) {
         tl.error("Something went wrong.");
